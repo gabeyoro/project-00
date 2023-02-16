9e79fae96b90d9a71c083b091526a23db44fc5b2
GYO <gabrielyoro@gmail.com>
2023-02-16 14:14:14 -0800

    removed testing api routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,43 +1,7 @@
 const router = require("express").Router();
-
 const { restoreUser } = require('../../utils/auth.js');
-const { requireAuth } = require('../../utils/auth.js');
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
 
 router.use(restoreUser);
 
-router.get(
-  '/restore-user',
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
-
-router.get(
-  '/require-auth',
-  requireAuth,
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
-
-router.get("/csrf/restore", (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({
-    "XSRF-Token": csrfToken,
-  });
-});
-
-router.get('/set-token-cookie', async (_req, res) => {
-  const user = await User.findOne({
-      where: {
-        username: 'Demo-lition'
-      }
-    });
-  setTokenCookie(res, user);
-  return res.json({ user: user });
-});
 
 module.exports = router;
