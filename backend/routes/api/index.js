f48f82e115473f5e41deb1b7531c73a7d2319afc
GYO <gabrielyoro@gmail.com>
2023-02-16 14:12:08 -0800

    added api testing routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,27 @@
 const router = require("express").Router();
 
+const { restoreUser } = require('../../utils/auth.js');
+const { requireAuth } = require('../../utils/auth.js');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+router.use(restoreUser);
+
+router.get(
+  '/restore-user',
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
 router.get("/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
   res.cookie("XSRF-TOKEN", csrfToken);
@@ -8,8 +30,14 @@ router.get("/csrf/restore", (req, res) => {
   });
 });
 
-router.post("/test", function (req, res) {
-  res.json({ requestBody: req.body });
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      }
+    });
+  setTokenCookie(res, user);
+  return res.json({ user: user });
 });
 
 module.exports = router;
