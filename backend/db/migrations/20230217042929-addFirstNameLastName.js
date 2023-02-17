7913b6f50aff9d0564937076ccb0b9d6434b1347
GYO <gabrielyoro@gmail.com>
2023-02-16 20:43:37 -0800

    schema change

--- backend/db/migrations/20230217042929-addFirstNameLastName.js
+++ backend/db/migrations/20230217042929-addFirstNameLastName.js
@@ -1,15 +1,19 @@
 'use strict';
+let options = {};
+options.tableName = 'Users'; 
+
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  
+}
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.addColumn('Users', 'firstName', {  // options object
       type: Sequelize.STRING(30),
-      allowNull: false,
     })
     await queryInterface.addColumn('Users', 'lastName', {  // options object
       type: Sequelize.STRING(30),
-      allowNull: false,
     })
   },
 
