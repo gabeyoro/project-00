ac9020d46e6508e71913be2c8a09120aeab3a90a
GYO <gabrielyoro@gmail.com>
2023-02-20 16:45:50 -0800

    added static associations

--- backend/db/models/attendance.js
+++ backend/db/models/attendance.js
@@ -5,6 +5,8 @@ const {
 module.exports = (sequelize, DataTypes) => {
   class Attendance extends Model {
     static associate(models) {
+      Attendance.belongsTo(models.Event, {foreignKey:"eventId"});
+      Attendance.belongsTo(models.User, {foreignKey:"userId"});
     }
   }
   Attendance.init({
