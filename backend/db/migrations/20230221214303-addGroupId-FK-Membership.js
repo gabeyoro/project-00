d3b118ffe97d8236c13618b56a2747bbd69b45af
GYO <gabrielyoro@gmail.com>
2023-02-21 13:44:41 -0800

    add groupId-FK-Membership

---
+++ backend/db/migrations/20230221214303-addGroupId-FK-Membership.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+options.tableName = "Memberships";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(options, "groupId", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "Groups",
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn(options, "groupId");
+  },
+};
